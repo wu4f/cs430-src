8ed25fb9f43411154807dad129b1a949c50200df
wuchang <wuchangfeng@bitbucket.org>
2018-08-08 11:47:03 -0700

    Guestbook v4 for Cloud Dataastore

---
+++ WebDev_Guestbook_v4_datastore/gbmodel/__init__.py
@@ -0,0 +1,14 @@
+#model_backend = 'pylist'
+#model_backend = 'sqlite3'
+model_backend = 'datastore'
+
+def get_model():
+    if model_backend == 'sqlite3':
+        from .model_sqlite3 import model
+    elif model_backend == 'pylist':
+        from .model_pylist import model
+    elif model_backend == 'datastore':
+        from .model_datastore import model
+    else:
+        raise ValueError("No appropriate databackend configured. ")
+    return model()
---
+++ WebDev_Guestbook_v4_datastore/gbmodel/model_sqlite3.py
@@ -0,0 +1,59 @@
+"""
+A simple guestbook flask app.
+ata is stored in a SQLite database that looks something like the following:
+
++------------+------------------+------------+----------------+
+| Name       | Email            | signed_on  | message        |
++============+==================+============+----------------+
+| John Doe   | jdoe@example.com | 2012-05-28 | Hello world    |
++------------+------------------+------------+----------------+
+
+This can be created with the following SQL (see bottom of this file):
+
+    create table guestbook (name text, email text, signed_on date, message);
+
+"""
+from datetime import date
+from .Model import Model
+import sqlite3
+DB_FILE = 'entries.db'    # file for our Database
+
+class model(Model):
+    def __init__(self):
+        # Make sure our database exists
+        connection = sqlite3.connect(DB_FILE)
+        cursor = connection.cursor()
+        try:
+            cursor.execute("select count(rowid) from guestbook")
+        except sqlite3.OperationalError:
+            cursor.execute("create table guestbook (name text, email text, signed_on date, message)")
+        cursor.close()
+
+    def select(self):
+        """
+        Gets all rows from the database
+        Each row contains: name, email, date, message
+        :return: List of lists containing all rows of database
+        """
+        connection = sqlite3.connect(DB_FILE)
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM guestbook")
+        return cursor.fetchall()
+
+    def insert(self, name, email, message):
+        """
+        Inserts entry into database
+        :param name: String
+        :param email: String
+        :param message: String
+        :return: True
+        :raises: Database errors on connection and insertion
+        """
+        params = {'name':name, 'email':email, 'date':date.today(), 'message':message}
+        connection = sqlite3.connect(DB_FILE)
+        cursor = connection.cursor()
+        cursor.execute("insert into guestbook (name, email, signed_on, message) VALUES (:name, :email, :date, :message)", params)
+
+        connection.commit()
+        cursor.close()
+        return True
