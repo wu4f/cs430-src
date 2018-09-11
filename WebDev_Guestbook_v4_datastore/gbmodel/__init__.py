5cd507d26e56ebde5d560a2fba1c1612dbce4988
Wu-chang Feng <wuchang@pdx.edu>
2018-09-11 08:48:59 -0700

    fix re-init bug on get_model

--- WebDev_Guestbook_v4_datastore/gbmodel/__init__.py
+++ WebDev_Guestbook_v4_datastore/gbmodel/__init__.py
@@ -2,13 +2,14 @@
 #model_backend = 'sqlite3'
 model_backend = 'datastore'
 
+if model_backend == 'sqlite3':
+    from .model_sqlite3 import model
+elif model_backend == 'pylist':
+    from .model_pylist import model
+elif model_backend == 'datastore':
+    from .model_datastore import model
+else:
+    raise ValueError("No appropriate databackend configured. ")
+
 def get_model():
-    if model_backend == 'sqlite3':
-        from .model_sqlite3 import model
-    elif model_backend == 'pylist':
-        from .model_pylist import model
-    elif model_backend == 'datastore':
-        from .model_datastore import model
-    else:
-        raise ValueError("No appropriate databackend configured. ")
-    return model()
+    return appmodel
