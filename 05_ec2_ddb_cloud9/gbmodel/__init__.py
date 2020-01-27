7bd17c1949b8a1c02779ae76d33f238f8c19c0af
Wu-chang Feng <wuchang@pdx.edu>
2020-01-27 12:15:24 -0800

    aws cleanup

--- 05_ec2_ddb_cloud9/gbmodel/__init__.py
+++ 05_ec2_ddb_cloud9/gbmodel/__init__.py
@@ -1,10 +1,7 @@
 #model_backend = 'pylist'
-#model_backend = 'sqlite3'
 model_backend = 'dynamodb'
 
-if model_backend == 'sqlite3':
-    from .model_sqlite3 import model
-elif model_backend == 'pylist':
+if model_backend == 'pylist':
     from .model_pylist import model
 elif model_backend == 'dynamodb':
     from .model_dynamodb import model
