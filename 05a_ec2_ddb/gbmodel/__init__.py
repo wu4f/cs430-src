f79b9d91ec9c2b88cdf6e8c48b87b5273e56bbbf
EC2 Default User <ec2-user@ip-172-31-26-76.us-west-2.compute.internal>
2020-01-27 17:47:30 +0000

    ddb version

--- 05a_ec2_ddb/gbmodel/__init__.py
+++ 05a_ec2_ddb/gbmodel/__init__.py
@@ -1,6 +1,7 @@
 #model_backend = 'pylist'
 #model_backend = 'sqlite3'
-model_backend = 'datastore'
+#model_backend = 'datastore'
+model_backend = 'dynamodb'
 
 if model_backend == 'sqlite3':
     from .model_sqlite3 import model
@@ -8,6 +9,8 @@ elif model_backend == 'pylist':
     from .model_pylist import model
 elif model_backend == 'datastore':
     from .model_datastore import model
+elif model_backend == 'dynamodb':
+    from .model_dynamodb import model
 else:
     raise ValueError("No appropriate databackend configured. ")
 
