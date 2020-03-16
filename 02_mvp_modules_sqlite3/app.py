f1f1692ed36f7ab5c0d1990be9e477435b68b004
Wu-chang Feng <wuchang@pdx.edu>
2020-03-16 15:16:29 -0700

    sqlite3 model wasn't being used.  port needs to be 8000 to run on linuxlab

--- 02_mvp_modules_sqlite3/app.py
+++ 02_mvp_modules_sqlite3/app.py
@@ -17,4 +17,4 @@ app.add_url_rule('/sign/',
                  methods=['GET', 'POST'])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
