5109c2c914f0b769a5957adeb7503af564414047
Wu-chang Feng <wuchang@pdx.edu>
2020-04-06 18:07:55 -0700

    move kubernetes deployment

--- 06_cloudrun/index.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from flask.views import MethodView
-import gbmodel
-
-class Index(MethodView):
-    def get(self):
-        model = gbmodel.get_model()
-        entries = [dict(name=row[0], email=row[1], signed_on=row[2], message=row[3] ) for row in model.select()]
-        return render_template('index.html',entries=entries)
