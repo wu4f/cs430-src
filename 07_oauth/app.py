50169ad798245417d515ac09f6e816dea84eae40
Wu-chang Feng <wuchang@pdx.edu>
2023-10-18 16:22:32 -0700

    remove unnecessary port

--- 07_oauth/app.py
+++ 07_oauth/app.py
@@ -29,4 +29,4 @@ app.add_url_rule('/logout',
                  methods=["GET"])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', debug=True)
