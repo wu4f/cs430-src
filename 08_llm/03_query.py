0d5fc6ba492f038e7381f264556991c3c0619b92
Wu-chang Feng <wuchang@pdx.edu>
2024-10-11 16:57:11 -0700

    openapi agent

--- 08_llm/03_query.py
+++ 08_llm/03_query.py
@@ -1,6 +1,6 @@
 from langchain.prompts import PromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_google_genai import GoogleGenerativeAI, GoogleGenerativeAIEmbeddings
