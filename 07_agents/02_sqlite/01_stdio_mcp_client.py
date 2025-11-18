799faccc43a6a7e78d7d83b10c9ea846792c7ec4
Wu-chang Feng <wuchang@pdx.edu>
2025-11-17 16:31:54 -0800

    langchain module moved

--- 07_agents/02_sqlite/01_stdio_mcp_client.py
+++ 07_agents/02_sqlite/01_stdio_mcp_client.py
@@ -1,6 +1,6 @@
 import os
 import readline
-from langgraph.prebuilt import create_react_agent
+from langchain.agents import create_agent
 from langchain_mcp_adapters.tools import load_mcp_tools
 from mcp import ClientSession
 from mcp.client.stdio import StdioServerParameters, stdio_client
